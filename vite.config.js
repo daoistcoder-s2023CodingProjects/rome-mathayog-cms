9b84d6c9a512648f0721dd5ddb41dc7b9d7dec56
daoistcoder <deservekindness@gmail.com>
2023-11-08 12:38:56 +0800

    UPDATE: vite config js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-// Load the APP_URL from an environment variable
-const APP_URL = import.meta.env.VITE_APP_URL;
-
 export default defineConfig({
     plugins: [
         laravel({
@@ -11,14 +8,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    server: {
-        proxy: {
-            // Proxy API requests to your Laravel application
-            '/api': {
-                target: APP_URL,
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-            },
-        },
-    },
 });
