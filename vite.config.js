8f7764230c80a3f404ffc648876d1b889cf50951
daoistcoder <deservekindness@gmail.com>
2023-11-08 13:09:45 +0800

    UPDATE: vite fix

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  define: {
+    'process.env.APP_URL': JSON.stringify('http://18.140.194.136'), // Set your desired APP_URL here
+  },
 });
