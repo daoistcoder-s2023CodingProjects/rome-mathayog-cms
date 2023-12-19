1fa230bbee90f55f6f1a0e058de9b85a9ec29e8d
daoistcoder <deservekindness@gmail.com>
2023-12-19 23:08:35 +0800

    UPDATE: stats-widget > dynamic to models

--- app/Filament/Widgets/StatsAdminOverview.php
+++ app/Filament/Widgets/StatsAdminOverview.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\ActQuestion;
+use App\Models\ExeQuestion;
+use App\Models\SumQuestion;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -10,9 +13,15 @@ class StatsAdminOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Unique views', '192.1k'),
-            Stat::make('Bounce rate', '21%'),
-            Stat::make('Average time on page', '3:12'),
+            Stat::make('Activity Questions', ActQuestion::query()->count())
+                ->description('All questions in activities')
+                ->color('success'),
+            Stat::make('Exercise Questions', ExeQuestion::query()->count())
+                ->description('All questions in exercises')
+                ->color('success'),
+            Stat::make('Summative Questions', SumQuestion::query()->count())
+                ->description('All questions in summative assesments')
+                ->color('success'),
         ];
     }
 }
