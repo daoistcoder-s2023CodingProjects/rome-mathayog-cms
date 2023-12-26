3b53356a9d718a40623c6251821d832cb4dac8aa
daoistcoder <deservekindness@gmail.com>
2023-12-27 05:29:40 +0800

    ADD: sort default to created_at

--- app/Filament/Resources/ImageResource.php
+++ app/Filament/Resources/ImageResource.php
@@ -98,7 +98,8 @@ class ImageResource extends Resource
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
-            ]);
+            ])
+            ->defaultSort('created_at', 'desc');
     }
 
     public static function getRelations(): array
